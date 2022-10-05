2dbc4f99c2f97ba727765453c28a713c391ce534
Neeraj Kumar Kannoujiya <96954081+neeraj7388011@users.noreply.github.com>
2022-10-05 13:24:04 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -133,3 +133,18 @@ STATICFILES_DIRS = [
 # media file configuration
 MEDIA_URL = '/media/'
 STATIC_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+
+# SMTP configuration
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'neerajkumar7388011@gmail.com'
+EMAIL_HOST_PASSWORD ='waeezpbwglpdwceq'
+EMAIL_USE_TLS = True
